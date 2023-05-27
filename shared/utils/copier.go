a6998fc8b5bf339b59bc378d64d34ac872f315c1
Sander Mendes <sandermendes@gmail.com>
2023-05-27 17:50:45 +0000

    Added function Copy to ignore empty fields

--- shared/utils/copier.go
+++ shared/utils/copier.go
@@ -1,8 +1,15 @@
 package utils
 
-import "github.com/jinzhu/copier"
+import (
+	"github.com/jinzhu/copier"
+)
 
 func Copy(to interface{}, from interface{}) error {
 	err := copier.Copy(to, from)
+	return err
+}
+
+func CopyIgnoreEmpty(to interface{}, from interface{}) error {
+	err := copier.CopyWithOption(to, from, copier.Option{IgnoreEmpty: true})
 	return err
 }
