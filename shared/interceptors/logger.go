9d45ea7c44aa34b12500635d32d60cf6832fbc97
Sander Mendes <sandermendes@gmail.com>
2023-05-27 17:48:29 +0000

    Started to add some color to console/terminal log :)

--- shared/interceptors/logger.go
+++ shared/interceptors/logger.go
@@ -11,6 +11,22 @@ import (
 	"google.golang.org/grpc/peer"
 )
 
+// Colors
+const (
+	Reset       = "\033[0m"
+	Red         = "\033[31m"
+	Green       = "\033[32m"
+	Yellow      = "\033[33m"
+	Blue        = "\033[34m"
+	Magenta     = "\033[35m"
+	Cyan        = "\033[36m"
+	White       = "\033[37m"
+	BlueBold    = "\033[34;1m"
+	MagentaBold = "\033[35;1m"
+	RedBold     = "\033[31;1m"
+	YellowBold  = "\033[33;1m"
+)
+
 func Logger(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	var ipAddr string
 	if p, of := peer.FromContext(ctx); of {
@@ -19,7 +35,7 @@ func Logger(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, ha
 		ipAddr = p.Addr.(*net.TCPAddr).IP.String()
 	}
 	fullMethod := strings.Split(info.FullMethod, "/")
-	fmt.Printf("%s - %s - Method - %s - %s\n", time.Now().Format(time.DateTime), ipAddr, fullMethod[1], fullMethod[2])
+	fmt.Printf(Green+"[%s] "+Reset+"- %s - Method - %s - %s\n", time.Now().Format(time.DateTime), ipAddr, fullMethod[1], fullMethod[2])
 	// fmt.Println("handler", handler)
 
 	resp, err := handler(ctx, req)
