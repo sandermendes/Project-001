d3d1dd531078defa1342f3ac8de67351346303a4
Sander Mendes <sandermendes@gmail.com>
2023-06-29 22:50:03 +0000

    expanded user test coverage with search function GetUser by ID or Email

--- microservices/user/server_test.go
+++ microservices/user/server_test.go
@@ -1,101 +1,123 @@
 package user_test
 
-// import (
-// 	"context"
-// 	"log"
+import (
+	"context"
+	"log"
 
-// 	. "github.com/onsi/ginkgo/v2"
-// 	. "github.com/onsi/gomega"
-// 	"github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/microservices/user"
-// 	userv1 "github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/shared/protobufs/_generated/user/v1"
-// 	serviceConnection "github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/shared/service_connection"
-// )
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/microservices/user"
+	userv1 "github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/shared/protobufs/_generated/user/v1"
+	serviceConnection "github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/shared/service_connection"
+)
 
-// const USER_SERVICE_PORT = "6010"
+const USER_SERVICE_PORT = "6010"
 
-// var _ = Describe("User", Ordered, func() {
-// 	var (
-// 		userConn userv1.UserServiceClient
-// 		err      error
-// 		ctx      context.Context
-// 	)
+var _ = Describe("User", Ordered, func() {
+	var (
+		userConn userv1.UserServiceClient
+		err      error
+		ctx      context.Context
+	)
 
-// 	BeforeAll(func() {
-// 		if err := user.ListenGRPC(USER_SERVICE_PORT); err == nil {
-// 			log.Fatal("server exited", err.Error())
-// 		}
-// 	})
+	BeforeAll(func() {
+		go user.ListenGRPC(USER_SERVICE_PORT)
 
-// 	BeforeAll(func() {
-// 		userConn, err = serviceConnection.GetUserConnection("localhost:" + USER_SERVICE_PORT)
-// 		if err != nil {
-// 			log.Fatalf("failed to connect to userService: %v", err)
-// 		}
+		defer func() {
+			userConn, err = serviceConnection.GetUserConnection("localhost:" + USER_SERVICE_PORT)
+			if err != nil {
+				log.Fatalf("failed to connect to userService: %v", err)
+			}
 
-// 		// userService = user.NewService()
-// 		ctx = context.TODO()
-// 	})
+			// userService = user.NewService()
+			ctx = context.TODO()
+		}()
+	})
 
-// 	It("Create user with invalid email", func() {
-// 		_, err := userConn.CreateUser(ctx, &userv1.CreateUserRequest{
-// 			FirstName: "John",
-// 			LastName:  "Doe",
-// 			Email:     "john-doe",
-// 			Password:  "123456",
-// 		})
+	It("Create user with invalid email", func() {
+		_, err := userConn.CreateUser(ctx, &userv1.CreateUserRequest{
+			FirstName: "John",
+			LastName:  "Doe",
+			Email:     "john-doe",
+			Password:  "123456",
+		})
 
-// 		Expect(err).Should(HaveOccurred())
-// 	})
+		Expect(err).Should(HaveOccurred())
+	})
 
-// 	// It("Create user with valid email", func() {
-// 	// 	userResponse, err := userService.CreateUser(ctx, &userv1.CreateUserRequest{
-// 	// 		FirstName: "Teste",
-// 	// 		LastName:  "Test",
-// 	// 		Email:     "john-doe@dummy-corp.com",
-// 	// 		Password:  "123456",
-// 	// 	})
-// 	// 	createdUserId := userResponse.ModelWithUUID.ID
+	It("Create user with valid email", func() {
+		userResponse, err := userConn.CreateUser(ctx, &userv1.CreateUserRequest{
+			FirstName: "Teste",
+			LastName:  "Test",
+			Email:     "john-doe@dummy-corp.com",
+			Password:  "123456",
+		})
+		createdUserId := userResponse.GetId()
 
-// 	// 	Expect(err).ShouldNot(HaveOccurred())
-// 	// 	Expect(userResponse).ToNot(BeNil())
-// 	// 	Expect(createdUserId).ShouldNot(BeEmpty())
-// 	// })
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(userResponse).ToNot(BeNil())
+		Expect(createdUserId).ShouldNot(BeEmpty())
+	})
 
-// 	// It("Update User", func() {
+	It("List User by Id or Email", func() {
 
-// 	// 	createUserResponse, _ := userService.CreateUser(ctx, &userv1.CreateUserRequest{
-// 	// 		FirstName: "update",
-// 	// 		LastName:  "Test",
-// 	// 		Email:     "update-test@dummy-corp.com",
-// 	// 		Password:  "123456",
-// 	// 	})
-// 	// 	createdUserId := createUserResponse.ModelWithUUID.ID
+		createUserResponse, _ := userConn.CreateUser(ctx, &userv1.CreateUserRequest{
+			FirstName: "List",
+			LastName:  "Test",
+			Email:     "list-test@dummy-corp.com",
+			Password:  "123456",
+		})
+		createdUserId := createUserResponse.GetId()
+		createdUserEmail := createUserResponse.GetEmail()
 
-// 	// 	updatedUserResponse, err := userService.UpdateUser(ctx, &userv1.UpdateUserRequest{
-// 	// 		Id:        createdUserId.String(),
-// 	// 		FirstName: "Teste Updated",
-// 	// 		LastName:  "Test Lastname",
-// 	// 		Email:     "update-test-1@dummy-corp.com",
-// 	// 	})
+		listByIdUserResponse, err := userConn.GetUser(ctx, &userv1.UpdateUserRequest{
+			Id: createdUserId,
+		})
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(listByIdUserResponse).ToNot(BeNil())
 
-// 	// 	Expect(err).ShouldNot(HaveOccurred())
-// 	// 	Expect(updatedUserResponse).ToNot(BeNil())
-// 	// })
+		listByEmailUserResponse, err := userConn.GetUser(ctx, &userv1.UpdateUserRequest{
+			Email: createdUserEmail,
+		})
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(listByEmailUserResponse).ToNot(BeNil())
+	})
 
-// 	// It("Delete User", func() {
-// 	// 	createUserResponse, _ := userService.CreateUser(ctx, &userv1.CreateUserRequest{
-// 	// 		FirstName: "update",
-// 	// 		LastName:  "Test",
-// 	// 		Email:     "delete-test@dummy-corp.com",
-// 	// 		Password:  "123456",
-// 	// 	})
-// 	// 	createdUserId := createUserResponse.ModelWithUUID.ID
+	It("Update User", func() {
 
-// 	// 	userResponse, err := userService.DeleteUser(ctx, &userv1.UpdateUserRequest{
-// 	// 		Id: createdUserId.String(),
-// 	// 	})
+		createUserResponse, _ := userConn.CreateUser(ctx, &userv1.CreateUserRequest{
+			FirstName: "update",
+			LastName:  "Test",
+			Email:     "update-test@dummy-corp.com",
+			Password:  "123456",
+		})
+		createdUserId := createUserResponse.GetId()
 
-// 	// 	Expect(err).ShouldNot(HaveOccurred())
-// 	// 	Expect(userResponse).ToNot(BeNil())
-// 	// })
-// })
+		updatedUserResponse, err := userConn.UpdateUser(ctx, &userv1.UpdateUserRequest{
+			Id:        createdUserId,
+			FirstName: "Teste Updated",
+			LastName:  "Test Lastname",
+			Email:     "update-test-1@dummy-corp.com",
+		})
+
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(updatedUserResponse).ToNot(BeNil())
+	})
+
+	It("Delete User", func() {
+		createUserResponse, _ := userConn.CreateUser(ctx, &userv1.CreateUserRequest{
+			FirstName: "update",
+			LastName:  "Test",
+			Email:     "delete-test@dummy-corp.com",
+			Password:  "123456",
+		})
+		createdUserId := createUserResponse.GetId()
+
+		userResponse, err := userConn.DeleteUser(ctx, &userv1.UpdateUserRequest{
+			Id: createdUserId,
+		})
+
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(userResponse).ToNot(BeNil())
+	})
+})
