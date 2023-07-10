08ca53cef4bff420e1d56e63d112492de8737888
Sander Mendes <sandermendes@gmail.com>
2023-07-10 17:54:55 +0000

    Started to implement/improve conversion of BirthDate and Gender

--- microservices/user/server.go
+++ microservices/user/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/providers/cache"
 	"github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/shared/interceptors"
 	userv1 "github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/shared/protobufs/_generated/user/v1"
 	"github.com/sandermendes/Go-Golang-Gorm-Postgres-Gqlgen-Graphql/main/shared/utils"
@@ -12,6 +13,10 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
+)
+
+var (
+	cacheService = cache.ConnectCache()
 )
 
 type Server struct {
@@ -54,6 +59,9 @@ func (s *Server) GetUser(ctx context.Context, input *userv1.UpdateUserRequest) (
 		return nil, status.Error(codes.Internal, "fail to return user data")
 	}
 	userResponse.Id = user.ID.String()
+	// TODO: Check for improve this conversion
+	// userResponse.BirthDate = (*string)(unsafe.Pointer(&user.BirthDate))
+	// userResponse.Gender = (*string)(unsafe.Pointer(&user.Gender))
 
 	return &userResponse, nil
 }
@@ -84,6 +92,10 @@ func (s *Server) UpdateUser(ctx context.Context, input *userv1.UpdateUserRequest
 		return nil, status.Error(codes.Internal, "fail to return user data")
 	}
 	userResponse.Id = user.ID.String()
+	// TODO: Need to implement clearing of user cache
+	// userResponse.BirthDate = (*string)(unsafe.Pointer(&user.BirthDate))
+	// userResponse.Gender = (*string)(unsafe.Pointer(&user.Gender))
+	// cacheService.Del(ctx, "UserId:"+userResponse.Id)
 
 	return &userResponse, nil
 }
